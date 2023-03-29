2bc42ce9126d3cc74536041334f2350f4ddd65d2
Alin Drăghici <guky667@gmail.com>
2023-03-29 04:15:46 +0300

    added spell cursors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pointerClassStart:string = "url('./assets/cursor/spells/";
+  pointerName:string = "";
+  pointerClassEnd:string = ".png'), pointer";
+
+  flunk:boolean = false;
+  lastSpell:string = "";
+
   play(id:string){
+    if(this.lastSpell == 'VOXAM' && id == 'BOOZNIK')
+    {
+      this.flunk = !this.flunk
+    }
+    this.lastSpell = id
+    if(!['BOOZNIK','LEXDOM','ZIMDOR'].includes(id))
+    {
+      this.pointerName = this.pointerClassStart + id + this.pointerClassEnd
+    }
+    
     var audio = new Audio('./assets/spells/audio/' + id + '.mp3');
     audio.loop = false;
     audio.play(); 
+    
+    setTimeout(()=>{ 
+      this.pointerName = ""
+      
+      if(this.flunk)
+      {
+        audio = new Audio('./assets/spells/audio/flunk.mp3');
+        audio.loop = false;
+        audio.play();
+      }
+    }, 2600)
+  }
+
+  getPointer(target:string = "")
+  {
+    if (this.pointerName.length != 0){
+      return { 'cursor' : this.pointerName }
+    }
+
+    if(target == 'idle')
+    {
+      return { 'cursor' : "url('../assets/cursor/idle.png'), pointer" }
+    }
+
+    return { 'cursor' : "url('../assets/cursor/active.png'), pointer" }
   }
 
 }
